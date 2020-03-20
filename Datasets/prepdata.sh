9fdcb44f2bd63a8322a7a6902fad4c288cecb7d8
Spacial <spacial@gmail.com>
2020-03-19 22:28:34 -0300

    need to ajust filenames

---
+++ Datasets/prepdata.sh
@@ -0,0 +1,212 @@
+#!/bin/bash
+#    prepare WHO data to plot by 0 day
+# Author: Jacson Querubin (email: see [1]
+# [1] $ echo "spacialATg-m-a-i-lDOTc-o-m" | sed -e 's/AT/@/g'  | sed -e 's/DOT/\./g' | sed -e 's/-//g'
+# Date: 18/03/2020
+# License: GPL v3 or superior
+#####
+# Deps:
+#####
+# Params:
+#     see usage() 
+#####
+# Changelog:
+# 20200318Z - initial version (0.1)
+#####
+# Wish/TODO:
+#####
+# Functions
+function usage() {
+        echo "Use: $0 inputfile.csv [countries.txt]"
+        echo 'Parameters: inputfile.csv [countries.txt]
+               - inputfile.csv ($1) - file from covid19 cases from WHO (csv format).
+               - [countries.txt] ($2) - file with selected countries (to narrow graphics), one per line.
+
+               output: data prepared to pandas/jupyter on screen
+        '
+        echo " * Tip: to debug script, add _DEBUG=on before running."       
+        return 0
+        exit
+}
+
+DEBUG(){
+        [ "$_DEBUG" == "on" ] && $@ || :
+}
+
+function saveenv(){
+    OLDDIR=$(pwd)
+    OLDLC_TYPE=${LC_TYPE}
+}
+
+function setenv(){
+    LC_CTYPE=C
+    TIME=$(date +"%H")
+    FULLTIME=$(date '+%Y/%m/%d %H:%M:%S')
+    createtmp
+    TMPDIR=$(echo ".tmp-${TMPNOW}")
+    DEBUG echo "mkdir -p ${TMPDIR}"
+    LOGFILE=${TMPDIR}/prepdata.log
+    mkdir -p ${TMPDIR}
+}
+
+function restorenv(){
+    cd ${OLDDIR}
+    LC_TYPE=${OLDLC_TYPE}
+    DEBUG echo "rm -rf ${TMPDIR}"
+    # rm -rf ${TMPDIR}
+}
+
+function createtmp(){
+    TMPNOW=$(date '+%Y%m%dT%H%M%S')
+    RANDATA=$(LANG=C tr -dc A-Za-z0-9 < /dev/urandom  | fold -w ${1:-16} | head -n 1)
+    TMPFILE=$(echo ${TMPNOW}${RANDATA})
+    DEBUG echo "${TMPNOW} // ${RANDATA}"
+    return 
+}
+
+function string_escape() {
+    local type
+    local chars
+    local string
+
+    if [[ "${1}" = "--type" ]] || [[ "${1}" = "-t" ]]; then
+        type="${2}"
+        shift 2
+    else
+        type="quote"
+    fi
+
+    case "${type}" in
+        quote)
+            chars=="'\""
+        ;;
+        regex)
+            chars=']$.*+\^?()['
+        ;;
+        *)
+            return 1
+        ;;
+    esac
+
+    if [[ -z "${1}" ]] && [ ! -t 0 ]; then
+        string=$(cat <&0)
+    else
+        string="${1}"
+    fi
+
+    echo "${string}" | sed -E "s/([${chars}])/\\\\\1/g"
+}
+
+
+function string_separator_camelcase(){
+    local separator="${1}"
+    local string="${2}"
+
+    if [[ -z "${separator}" ]]; then
+        return 1
+    fi
+
+    if [[ -z "${string}" ]] && [ ! -t 0 ]; then
+        string=$(cat <&0)
+    fi
+
+    separator=$(string_escape --type regex "${1}" | sed 's#/#\\/#g')
+
+    echo "${string}" | sed -E "s/${separator}(\w)/\u\1/g"
+}
+
+function string_camelcase_underscore() {
+    local string="${1}"
+
+    if [[ -z "${string}" ]] && [ ! -t 0 ]; then
+        string=$(cat <&0)
+    fi
+
+    string_camelcase_separator "_" "${string}"
+}
+
+function string_camelcase_separator() {
+    local separator="${1}"
+    local string="${2}"
+    local pattern
+
+    if [[ -z "${separator}" ]]; then
+        return 1
+    fi
+
+    if [[ -z "${string}" ]] && [ ! -t 0 ]; then
+        string=$(cat <&0)
+    fi
+
+    separator=$(string_escape --type regex "${1}" | sed 's#/#\\/#g')
+    pattern="s/([A-Za-z0-9])([A-Z])/\1${separator}\2/g"
+
+    echo "${string}" | sed -E "${pattern}" | sed -E "${pattern}"
+}
+
+
+function getcountries(){
+    DEBUG echo "getting countries from: ${OLDDIR}/${INPUTFILE}"
+    DEBUG echo "putting in: ${TMPDIR}/${TMPFILE}"
+    cat ${OLDDIR}/${INPUTFILE} | cut -d',' -f2 | grep -v -e '^[[:space:]]*$' | grep -v 'location' | sort | uniq > ${TMPDIR}/${TMPFILE}
+    # testar código de retorno
+    COUNTRIESFILE=$(echo "${TMPDIR}/${TMPFILE}")
+}
+
+function forkcountries(){
+    TOTAL=$(cat ${TMPDIR}/${TMPFILE} | wc -l)
+    DEBUG echo "Total countries in ${TMPDIR}/${TMPFILE} is ${TOTAL}, forking files.."
+    # for c in $(cat ${TMPDIR}/${TMPFILE});
+    IFS=''
+    while read c; 
+    do 
+        NEW=$(string_camelcase_underscore ${c})
+        DEBUG echo "creating ${c} file: ${TMPDIR}/raw_${NEW}_data.csv"
+        grep ${c} ${OLDDIR}/${INPUTFILE} > ${TMPDIR}/raw_${NEW}_data.csv
+    done < ${TMPDIR}/${TMPFILE}
+    TOTFILES=$(ls -l ${TMPDIR}/raw_*.csv | wc -l)
+    DEBUG echo "There were ${TOTFILES} created"
+}
+
+#Variáveis
+saveenv
+setenv
+
+#testing me
+if (( EUID == 0 )); then
+   echo "Running as root??? no way! bye." 1>&2
+   exit 100
+fi
+
+## Validating ags
+if [ "$#" -eq 2 ]; then
+        INPUTFILE=${1}
+        COUNTRIESFILE=${2}
+        DEBUG echo "countriesfile seted to: ${COUNTRIESFILE}"
+fi
+
+if [ ! "$#" -ge 1 ]; then
+        usage
+        exit 1
+else
+        INPUTFILE=${1}
+        DEBUG echo "no countriesfile seted"
+fi
+
+
+DEBUG echo "inputfile seted to: ${INPUTFILE}"
+DEBUG echo "tmpdir: /tmp/${TMPNOW}"
+DEBUG echo "tmpfile: ${TMPFILE}"
+
+if [ "${COUNTRIESFILE}" == "" ]; then
+    getcountries
+fi
+
+DEBUG echo "2x - countriesfile seted to: ${COUNTRIESFILE}"
+
+# forkcountries
+string_separator_camelcase "-" "This is a string" 
+string_camelcase_separator "d" "This Is A String" # This_Is_A_String
+
+
+restorenv
--- README.md
+++ README.md
@@ -10,8 +10,9 @@ Resources about Covid-19, data, links, repos, and a lot of stuff
 - [eSIR](https://github.com/lilywang1988/eSIR): R package that extends state-space SIR epidemiological models
 - [coronavirus-tracker-cli](https://github.com/sagarkarira/coronavirus-tracker-cli/): Track conronavirus cases from command line.
 - [COVID-19](https://github.com/CSSEGISandData/COVID-19): 2019 Novel Coronavirus COVID-19 (2019-nCoV) Data Repository by Johns Hopkins CSSE
+- [COVID-19 Scenarios](https://neherlab.org/covid19/): This output of a mathematical model depends on model assumptions and parameter choices. We have done our best (in limited time) to check the model implementation is correct. Please carefully consider the parameters you choose and interpret the output with caution. By [neherlab](https://neherlab.org) - [repo](https://github.com/neherlab/covid19_scenarios)
 
-## Links
+## Outbreaks and Pandemics
 
 - [Coronavirus COVID-19 Global Cases](https://coronavirus.jhu.edu/map.html) by The Center for Systems Science and Engineering (CSSE) at Johns Hopkins University
 - [Outbreak](https://meltingasphalt.com/interactive/outbreak/) by Kevin Simler
@@ -23,14 +24,27 @@ Resources about Covid-19, data, links, repos, and a lot of stuff
   - [Brazil dashboard](https://nucleuswealth.com/coronavirus-covid19-dashboard-charts-and-data-every-country/brazil)
 - [Don't be Italy](https://observablehq.com/@yurivish/dont-be-italy)
 - [11 charts that explain the coronavirus pandemic](https://www.vox.com/future-perfect/2020/3/12/21172040/coronavirus-covid-19-virus-charts)
+- [US Health Weather Map](https://healthweather.us)
+- [Tracking the spread of the novel coronavirus](https://graphics.reuters.com/CHINA-HEALTH-MAP/0100B59S39E/index.html)
+- [COVID-19’s exponential growth](https://graphics.reuters.com/HEALTH-CORONAVIRUS-GROWTH/0100B5KL438/)
+
+### Related Topics
+
 - [Detection of Airborne Viruses](https://www.kth.se/mst/research/sensors/project/detection-airborne-1.523039)
 - [Rationing, Data, and the Ethics of our Decisions](https://medium.com/nightingale/rationing-data-and-the-ethics-of-our-decisions-f3c65ea3fd75)
+- [A Case Report in @NatureMedicine shows that #COVID19 is not a laboratory construct or a purposefully manipulated virus.](https://twitter.com/nresearchnews/status/1239952195535147008). [report](https://www.nature.com/articles/s41591-020-0820-9)
+- [Open Source Ventilator - OpenLung BVM Ventilator](https://gitlab.com/TrevorSmale/OSV-OpenLung)
 
 ### Portuguese
 
 - [Com um 'bullshiter' no Planalto, covid-19 pode virar a peste negra brasileira](https://www.dw.com/pt-br/com-umbullshiterno-planalto-covid-19-pode-virar-a-peste-negra-brasileira/a-52809685)
+- [Covid Zero](http://covidzero.com.br/como-ajudar/): DADOS EM TEMPO REAL PARA ERRADICAÇÃO DO CORONAVIRUS
 
 ## Other stuff
 
 - [How to Make Your Own Hand Sanitizer](https://www.wired.com/story/how-to-make-hand-sanitizer/)
 - [JHUP ANNOUNCES FREE ACCESS TO ITS JOURNALS AND BOOKS ON PROJECT MUSE DURING COVID-19 CRISIS](https://www.press.jhu.edu/news/announcements/jhup-announces-free-access-its-journals-and-books-project-muse-during-covid-19)
+- [COVID-19 Open Research Dataset (CORD-19)](https://pages.semanticscholar.org/coronavirus-research)
+- [Patriarch of Pandemics](https://demystifyingscience.com/blog/2020/3/4/patriarch-of-pandemics)
+- [Open Source Ventilator, OpenLung Projects Aim to Address the COVID-19 Ventilator Shortfall](https://www.hackster.io/news/open-source-ventilator-openlung-projects-aim-to-address-the-covid-19-ventilator-shortfall-c7a5ee2f8e58)
+- [NANOHACK, THE OPEN SOURCE RESPIRATOR MASK](https://copper3d.com/hackthepandemic/)
